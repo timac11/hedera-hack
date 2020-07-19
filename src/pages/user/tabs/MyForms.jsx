33bae700876e2ce09eadf5656775d0df5b9f82d9
aleksandrsokolov <timac11@bk.ru>
2020-07-20 01:36:11 +0300

    minor fix

--- src/pages/user/tabs/MyForms.jsx
+++ src/pages/user/tabs/MyForms.jsx
@@ -93,7 +93,7 @@ class MyForms extends React.Component {
                onOk={this.shareToUsers}
                onCancel={this.handleShareClose}
         >
-          <p>Chose user to share</p>
+          <p>Choose users to share</p>
           <Select style={{width: 200}}
                   mode="multiple"
                   onChange={(val) => this.setState({chosenUser: val})}>
